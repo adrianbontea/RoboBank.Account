f36653bca1a5ef4a9abecb33ee9f3c18ec7e12d7
adrianbontea <contact@adrianbontea.com>
2017-01-14 15:09:48 +0200

    Stub implementation of IAccountRepository and ICardRepository

---
+++ src/RoboBank.Account.Application.Adapters.NetStandard/StubAccountRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RoboBank.Account.Application.Ports;
+using System.Text;
+
+namespace RoboBank.Account.Application.Adapters.NetStandard
+{
+    public class StubAccountRepository : IAccountRepository
+    {
+        private const string OwnCustomerId = "RoboBank";
+        private readonly IEnumerable<Domain.Account> _cache = new List<Domain.Account>
+        {
+            new Domain.Account
+            {
+                Id = "account1",
+                CustomerId ="customer1",
+                Currency = "USD",
+                Balance = 1000
+            } ,
+            new Domain.Account
+            {
+                Id = "account2",
+                CustomerId = "customer1",
+                Currency = "EUR",
+                Balance = 1000
+            }
+        };
+
+        public async Task<IEnumerable<Domain.Account>> GetByCustomerIdAsync(string customerId)
+        {
+            return _cache.Where(acc => acc.CustomerId == customerId).ToArray();
+        }
+
+        public async Task<Domain.Account> GetByIdAsync(string id)
+        {
+            return _cache.FirstOrDefault(acc => acc.Id == id);
+        }
+
+        public async Task<Domain.Account> GetOwnAccountByCurrencyAsync(string currency)
+        {
+            return _cache.FirstOrDefault(acc => acc.CustomerId == OwnCustomerId && acc.Currency == currency);
+        }
+    }
+}
---
+++ src/RoboBank.Account.Application.Adapters.NetStandard/StubCardRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RoboBank.Account.Application.Ports;
+using RoboBank.Account.Domain;
+using System.Text;
+
+namespace RoboBank.Account.Application.Adapters.NetStandard
+{
+    public class StubCardRepository: ICardRepository
+    {
+        private readonly IEnumerable<Card> _cache = new List<Card>
+        {
+            new Card
+            {
+                Id = "card1",
+                Name = "John Doe",
+                ExpirationDate = new DateTime(2020, 4, 10),
+                CVV = 123,
+                Account = new Domain.Account
+                {
+                   Id = "account1",
+                   CustomerId ="customer1",
+                   Currency = "USD",
+                   Balance = 1000
+                }
+            }
+        };
+        public async Task<Card> GetByIdAsync(string id)
+        {
+            return _cache.FirstOrDefault(card => card.Id == id);
+        }
+    }
+}
